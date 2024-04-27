1af2d32d1874c50f191741d78e36383acc962ef0
김용민 <dydals3440@gmail.com>
2024-04-27 19:56:58 +0900

    feat: Multer 세팅하기

---
+++ src/common/const/path.const.ts
@@ -0,0 +1,19 @@
+import { join } from "path";
+
+// 서버 프로젝트의 루트 폴더 (cwd 현재 서버를 실행한 위치 NEST_SNS 폴더의 절대경로)
+export const PROJECT_ROOT_PATH = process.cwd();
+// 외부에서 접근 가능한 파일들을 모아둔 폴더 이름.
+export const PUBLIC_FOLDER_NAME = "public";
+// 포스트 이미지들을 저장할 폴더 이름.
+export const POSTS_FOLDER_NAME = "posts";
+
+// 실제 공개폴더의 절대 경로
+// /${프로젝트의 위치}/public
+export const PUBLIC_FOLDER_PATH = join(PROJECT_ROOT_PATH, PUBLIC_FOLDER_NAME);
+
+// 포스트 이미지룰 저장할 폴더
+export const POST_IMAGE_PATH = join(PUBLIC_FOLDER_PATH, POSTS_FOLDER_NAME);
+
+// 절대경로 x
+// 앞에 경로는 사용자가 붙일 수 있게/public/posts/xxx.jpg
+export const POST_PUBLIC_IMAGE_PATH = join(PUBLIC_FOLDER_NAME, POSTS_FOLDER_NAME);
--- src/posts/entities/posts.entity.ts
+++ src/posts/entities/posts.entity.ts
@@ -1,35 +1,41 @@
-import { IsString } from 'class-validator';
-import { BaseModel } from 'src/common/entity/base.entity';
-import { stringValidationMessage } from 'src/common/validation-message/string-validation-message';
-import { UsersModel } from 'src/users/entities/users.entity';
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { IsString } from "class-validator";
+import { BaseModel } from "src/common/entity/base.entity";
+import { stringValidationMessage } from "src/common/validation-message/string-validation-message";
+import { UsersModel } from "src/users/entities/users.entity";
+import { Column, Entity, ManyToOne } from "typeorm";
 
 @Entity()
 export class PostsModel extends BaseModel {
-  // 1) UsersModel과 연동 FK를 이용해서
-  // 2) null이 될 수 없다.
-  @ManyToOne(() => UsersModel, (user) => user.posts, {
-    nullable: false,
-  })
-  author: UsersModel;
-  // @Column()
-  // author: string;
+    // 1) UsersModel과 연동 FK를 이용해서
+    // 2) null이 될 수 없다.
+    @ManyToOne(() => UsersModel, (user) => user.posts, {
+        nullable: false,
+    })
+    author: UsersModel;
+    // @Column()
+    // author: string;
 
-  @Column()
-  @IsString({
-    message: stringValidationMessage,
-  })
-  title: string;
+    @Column()
+    @IsString({
+        message: stringValidationMessage,
+    })
+    title: string;
 
-  @Column()
-  @IsString({
-    message: stringValidationMessage,
-  })
-  content: string;
+    @Column()
+    @IsString({
+        message: stringValidationMessage,
+    })
+    content: string;
 
-  @Column()
-  likeCount: number;
+    // 이미지 필수가 아니게
+    @Column({
+        nullable: true,
+    })
+    image?: string;
 
-  @Column()
-  commentCount: number;
+    @Column()
+    likeCount: number;
+
+    @Column()
+    commentCount: number;
 }
