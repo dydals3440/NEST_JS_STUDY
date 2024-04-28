cd90d90c19c10d8de64299a94437963ff1fca39a
김용민 <dydals3440@gmail.com>
2024-04-28 19:48:23 +0900

    feat: broadcast 기능

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -20,30 +20,23 @@ export class ChatsGateway implements OnGatewayConnection {
     @WebSocketServer()
     server: Server;
 
-    // handleConnection은 연결이 되었을 떄 실행됨.
     handleConnection(socket: Socket) {
         console.log(`on connect called : ${socket.id}`);
     }
 
     @SubscribeMessage("enter_chat")
     enterChat(
-        // 방의 ID들을 리스트로 받는 이유: 하나의 방만 join이 아닌, 여러개의 방을 join하고 싶을 수 있기 여러번 보내면 안좋을 것 같으니,
-        // 한번에 여러개 방들을 받을 수 있게
         @MessageBody() data: number[],
-        // 위에 인자로 받은 socket (현재 함수에 연결된 소켓 가져오는 방법)
+
         @ConnectedSocket() socket: Socket,
     ) {
         for (const chatId of data) {
-            // socket.join() -> 방에 들어가는 이유
-            // join하는 방 이름은 무조건 string
             socket.join(chatId.toString());
         }
     }
 
     @SubscribeMessage("send_message")
     sendMessage(@MessageBody() message: { message: string; chatId: number }, @ConnectedSocket() socket: Socket) {
-        // this.server.emit("receive_message", "hello from server");
-        // 특정 채팅방에만 보내고 싶을떄
-        this.server.in(message.chatId.toString()).emit("receive_message", message.message);
+        socket.to(message.chatId.toString()).emit("receive_message", message.message);
     }
 }
