7e1a7d1f9ab611028432ca02f4da0dc3192054d2
김용민 <dydals3440@gmail.com>
2024-04-28 20:42:11 +0900

    feat: Paginate Chat API 생성하기

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -4,10 +4,11 @@ import { ChatsService } from "./chats.service";
 import { ChatsController } from "./chats.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ChatsModel } from "./entity/chats.entity";
+import { CommonModule } from "src/common/common.module";
 
 @Module({
     // 모델만들면 꼮 import
-    imports: [TypeOrmModule.forFeature([ChatsModel])],
+    imports: [TypeOrmModule.forFeature([ChatsModel]), CommonModule],
     controllers: [ChatsController],
     // gateway는 컨트롤러에 등록
     providers: [ChatsGateway, ChatsService],
